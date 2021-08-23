6d5b492dc139dcf9e702b4466901e398655e9572
misokatsusand <dam_do_4646@yahoo.co.jp>
2021-08-23 14:44:55 +0900

    functional repair

--- memo.rb
+++ memo.rb
@@ -10,7 +10,6 @@ def result(file_name) #実行結果の表示
   puts CSV.read(file_name) #csv読み込み
 end
 
-
 ###############
 # 本処理
 ###############
@@ -21,41 +20,64 @@ loop do #1,2以外が入力されたときの再入力用のloop
 
   #ifで1,2ごとの処理を決定
   #1,2以外はloopで再入力
-  if memo_type == 1
+  if memo_type == 1 #メモを新規作成
     puts "新規でメモを作成します..."
 
     #ファイル名を入力
     puts "拡張子を除いたファイル名を入力してください"
     file_name = "#{gets.chomp}.csv" #ファイル名取得用の変数
+    search_file = Dir.glob("./#{file_name}") #入力したファイル名が既存か検索
 
-    #ファイルの内容を入力
-    puts "メモの内容を入力してください(Enterでその行を確定します)"
-    puts "Ctrl+Dで入力を終了します"
-    CSV.open(file_name,"w")do |csv| #csv書き込み"wモード"
-      csv << [STDIN.read] #csvへ標準入力
+    if search_file.empty? #入力したファイル名が存在しないとき
+      #ファイルの内容を入力
+      puts "メモの内容を入力してください(Enterでその行を確定します)"
+      puts "Ctrl+Dで入力を終了します"
+      CSV.open(file_name,"w")do |csv| #csv書き込み"wモード"
+        csv << [STDIN.read] #csvへ標準入力
+      end
+
+      result(file_name) #実行結果呼び出し
+
+      break #loop終了
+
+    else #入力したファイル名が存在するとき
+      puts search_file
+      puts "入力したファイル名はすでに存在します"
+      puts "処理を終了します..."
+
+      break #loop終了
     end
 
-    result(file_name) #実行結果呼び出し
-
-    break #loop終了
-
-  elsif memo_type == 2
+  elsif memo_type == 2 #既存のメモを編集
     puts "既存のメモを編集します..."
 
     #ファイル名を入力
     puts "編集するファイル名を拡張子を除いて入力してください"
-    file_name = "#{gets.chomp}.csv"
+    file_name = "#{gets.chomp}.csv" #ファイル名取得用の変数
+    search_file = Dir.glob("./#{file_name}") #入力したファイル名が既存か検索
 
-    #ファイルの内容を入力
-    puts "メモの内容を入力してください(改行も入力に含みます)"
-    puts "Ctrl+Dで入力を終了します"
-    CSV.open(file_name,"a")do |csv| #csv追記"aモード"
-      csv << [STDIN.read] #csvへ標準入力
+    if !(search_file.empty?) #入力したファイル名が存在するとき
+      puts "以下のファイルが見つかりました。このファイルにメモを追加します"
+      puts search_file
+
+      #ファイルの内容を入力
+      puts "メモの内容を入力してください(改行も入力に含みます)"
+      puts "Ctrl+Dで入力を終了します"
+      CSV.open(file_name,"a")do |csv| #csv追記"aモード"
+        csv << [STDIN.read] #csvへ標準入力
+      end
+
+      result(file_name) #実行結果呼び出し
+
+      break #loop終了
+
+    else #入力したファイル名が存在しないとき
+      puts "該当するファイル名がありません。"
+      puts "処理を終了します..."
+
+      break #loop終了
+
     end
-
-    result(file_name) #実行結果呼び出し
-
-    break #loop終了
 
   else
     #再入力を促し再loop
